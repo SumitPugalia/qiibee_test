5761cc99b4a1b7d16e8f87a6bf61b095fbcef1be
Sumit Pugalia <sumit@cafu.com>
2021-07-17 16:01:43 +0400

    add endpoints and auth plug

---
+++ apps/qiibee_web/lib/qiibee_web/plugs/validate_token.ex
@@ -0,0 +1,46 @@
+defmodule QiibeeWeb.Plugs.ValidateToken do
+    @moduledoc """
+    Authenticate the bearer token.
+    """
+  
+    @behaviour Plug
+  
+    import Plug.Conn
+    import Phoenix.Controller
+    alias Qiibee.Accounts
+  
+    @impl Plug
+    def init(opts), do: opts
+  
+    @impl Plug
+    def call(conn, _) do
+      with {:ok, token} <- fetch_token(conn),
+           {:ok, user} <- Accounts.validate_user(token) do
+        conn
+        |> assign(:current_user, user)
+      else
+        {:error, :bearer_token_not_found} ->
+            msg = "Bearer Token Not Found"
+            send_error(conn, msg)
+        _ -> 
+            msg = "Invalid Token"
+            send_error(conn, msg)
+      end
+    end
+
+    defp send_error(conn, msg) do
+        conn
+        |> put_status(:unauthorized)
+        |> put_view(QiibeeWeb.ErrorView)
+        |> render("401.json", message: msg)
+        |> halt
+    end
+  
+    defp fetch_token(conn) do
+      case get_req_header(conn, "authorization") do
+        ["Bearer " <> token] -> {:ok, token}
+        _ ->
+          {:error, :bearer_token_not_found}
+      end
+    end
+  end
--- apps/qiibee_web/lib/qiibee_web/router.ex
+++ apps/qiibee_web/lib/qiibee_web/router.ex
@@ -5,7 +5,39 @@ defmodule QiibeeWeb.Router do
     plug :accepts, ["json"]
   end
 
-  scope "/api", QiibeeWeb do
+  pipeline :validate_token do
+    plug QiibeeWeb.Plugs.ValidateToken
+  end
+
+  pipeline :validate_api_key do
+    plug QiibeeWeb.Plugs.ValidateApiKey
+  end
+
+
+  scope "/api" do
     pipe_through :api
+
+    scope "/users", QiibeeWeb do
+      post "/register", AccountsController, :register
+    end
+
+    scope "/users", QiibeeWeb do
+      pipe_through :validate_token
+
+      patch "/redeem_coupon/:code", AccountsController, :redeem_coupon
+      patch "/redeem_reward/:patch", AccountsController, :redeem_reward
+      get "/transaction_history", TransactionController, :history
+
+    end
+
+    scope "/admin/users/:user_id", QiibeeWeb.Admin do
+      pipe_through :validate_api_key
+
+      get "/balance", AccountsController, :balance
+      patch "/:points/add_points", AccountsController, :add_points
+      patch "/:points/deduct_points", AccountsController, :deduct_points
+      get "/transaction_history", TransactionController, :history
+
+    end
   end
 end
